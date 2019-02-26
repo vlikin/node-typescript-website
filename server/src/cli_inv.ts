db4f3bbd1b34b94723446aebaca070b37d2918f8
Viktor <viktor.shelepen@basechord.com>
2019-02-26 18:03:51 +0200

    Inversify commander utils integration.

---
+++ server/src/cli_inv.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import {group, action, build, registerGroups} from 'inversify-commander-utils';
+import {Container, inject, injectable} from 'inversify';
+import program from 'commander';
+
+@group('printer')
+class TestGroup {
+
+  @inject(TodoContainer)
+  public todoContainer!: TodoContainer;
+
+  @action(
+    'A <parameter>',
+    [
+      { pattern: '-c, --count <mode>', description: 'Number of prints.' }
+    ]
+  )
+  public testA(parameter: string, command: any) {
+    console.log(this.todoContainer.printPaper());
+    console.log(parameter, command.count);
+  }
+
+}
+
+const container = new Container();
+container.bind(TodoContainer).to(TodoContainer);
+registerGroups(container);
+build(program, container);
